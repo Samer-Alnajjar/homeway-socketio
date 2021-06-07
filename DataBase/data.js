eab0902bbfab541e2574116881f2093eb1ad5ecb
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-06-07 18:43:37 +0300

    Fin structure

--- DataBase/data.js
+++ DataBase/data.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 module.exports = client;
